c28371bfd2e770f9027deb31323586f2b92abda4
Raphael Rios <rios.raph@gmail.com>
2025-10-23 21:39:24 -0300

    feat: last implementations for first product microservice

--- src/main/java/br/com/dio/storefront/mapper/IProductMapper.java
+++ src/main/java/br/com/dio/storefront/mapper/IProductMapper.java
@@ -1,10 +1,17 @@
 package br.com.dio.storefront.mapper;
 
+import br.com.dio.storefront.controller.request.ProductSaveRequest;
+import br.com.dio.storefront.controller.response.ProductAvailableResponse;
+import br.com.dio.storefront.controller.response.ProductDetailResponse;
+import br.com.dio.storefront.controller.response.ProductSavedResponse;
+import br.com.dio.storefront.dto.ProductDetailDTO;
 import br.com.dio.storefront.dto.ProductInfoDTO;
 import br.com.dio.storefront.entity.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -13,4 +20,12 @@ public interface IProductMapper {
 
     ProductInfoDTO toDTO(final ProductEntity entity, final BigDecimal price);
 
+    @Mapping(target = "active", constant = "false")
+    ProductEntity toEntity(final ProductSaveRequest request);
+
+    ProductSavedResponse toResponse(final ProductEntity entity);
+
+    List<ProductAvailableResponse> toResponse(final List<ProductEntity> entities);
+
+    ProductDetailResponse toResponse(final ProductInfoDTO dto);
 }
